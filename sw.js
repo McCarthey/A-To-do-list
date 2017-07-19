8ac8cac27dcc8da129d22a795acc2645a8588896
McCarthey <mccarthey123@outlook.com>
2017-07-19 11:01:07 +0800

    PWA

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 'use strict'
 importScripts('sw-toolbox.js');
-toolbox.precache(['index.html', 'style.css', 'index.js']);
+toolbox.precache(['index.html', 'styles.css', 'index.js']);
 toolbox.router.get('/images/*', toolbox.cacheFirst);
 toolbox.router.get('/*', toolbox.networkFirst, {
 	networkTimeoutSeconds: 5
